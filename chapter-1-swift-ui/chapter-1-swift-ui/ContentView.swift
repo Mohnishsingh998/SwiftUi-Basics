86b74801d4b9c8866e3a2ae9b3653a516611724a
mohnishsingh yadav <132808264+Mohnishsingh998@users.noreply.github.com>
2026-01-05 00:19:46 +0530

    Modifed the view and learned about the @State,var,let,toogel,tapgesture

--- chapter-1-swift-ui/chapter-1-swift-ui/ContentView.swift
+++ chapter-1-swift-ui/chapter-1-swift-ui/ContentView.swift
@@ -2,17 +2,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let ColorList1 : [Color] = [
+            .green,
+            .red,
+            .blue,
+            .purple,
+            .black,
+            .cyan
+    ]
+    let ColorList2 : [Color] = [
+        .yellow,
+        .black,
+        .orange,
+        .cyan,
+        .indigo,
+        .red
+    ]
+    ///static varialble used for the mutalbe componnts or parts in the views
+    @State private var screenTap : Bool = false;
+    @State private var offsetY : CGFloat = -1000.0
     var body: some View {
         ZStack {
             LinearGradient(
-                colors: [
-                    .green,
-                    .red,
-                    .blue,
-                    .purple,
-                    .black,
-                    .cyan
-                ],
+                colors: screenTap ? ColorList1 : ColorList2,
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             )
@@ -23,6 +35,13 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                     .foregroundStyle(.white)
+                    .offset( y : offsetY)
+            }
+        }
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 1)){
+                screenTap.toggle()
+                offsetY = screenTap ? 0 : -1000.0
             }
         }
     }
